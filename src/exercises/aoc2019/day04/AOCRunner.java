4f63f79a919d5e238be01d6f9445af5d253567f5
Durb0 <thomasnou@orange.fr>
2023-06-20 13:32:57 +0200

    ♻️ y2019d4

--- src/exercises/aoc2019/day04/AOCRunner.java
+++ src/exercises/aoc2019/day04/AOCRunner.java
@@ -6,8 +6,8 @@ import utilities.Printer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.regex.*;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -32,57 +32,53 @@ public class AOCRunner extends A_AOC {
     }
 
     @Override
-    public void run(){
+    public void run() {
         List<Integer> interval = parseInput();
 
         long start1 = System.currentTimeMillis();
-        List<Integer> s1 = countValidPasswords(interval, false);
+        List<Integer> s1 = getValidPasswords(interval, false);
         long end1 = System.currentTimeMillis();
         Printer.println("temps s1 -> " + (end1 - start1));
 
         solution1 = s1.size();
-        solution2 = countValidPasswords(interval, true).size();
+        solution2 = getValidPasswords(interval, true).size();
 
         Printer.println(solution2);
     }
 
-    private List<Integer> countValidPasswords(List<Integer> interval, boolean s2){
+    private List<Integer> getValidPasswords(List<Integer> interval, boolean s2) {
         int n = interval.get(0);
-        List<Integer> count = new ArrayList<>(List.of());
-        while(n <= interval.get(1)){
-            boolean ci = checkIncrease(n);
-            boolean cd = s2 ? checkDoublon2(n) : checkDoublon(n);
-            if(!ci || !cd){
-                if(!ci){
-                    n = applyIncrease(n);
-                    Printer.println("increase de n -> "+ n);
-                }
-                else{
-                    n = s2 ? applyDoublon2(n) : applyDoublon(n);
-                    Printer.println("ajout d'un doublon à n -> " + n);
-                }
-            }
-            else{
+        List<Integer> validPasswords = new ArrayList<>(List.of());
+        while (n <= interval.get(1)) {
+            boolean checkIncrease = checkIncrease(n);
+            boolean checkDoublon = s2 ? checkDoublon2(n) : checkDoublon(n);
+            if (checkIncrease && checkDoublon) {
                 Printer.println("valid -> " + n);
-                count.add(n);
+                validPasswords.add(n);
                 n++;
             }
-
+            else if (!checkIncrease) {
+                n = applyIncrease(n);
+                Printer.println("increase de n -> " + n);
+            }
+            else {
+                n = s2 ? applyDoublon2(n) : applyDoublon(n);
+                Printer.println("ajout d'un doublon à n -> " + n);
+            }
         }
-        return count;
+        return validPasswords;
     }
 
-    private int applyIncrease(int n){
+    private int applyIncrease(int n) {
         List<Integer> digits = intToList(n);
         int valuePlate = -1;
-        for (int i = 1; i < digits.size(); i++){
-            if(valuePlate == -1){
-                if(digits.get(i) < digits.get(i-1)){
-                    valuePlate = digits.get(i-1);
+        for (int i = 1; i < digits.size(); i++) {
+            if (valuePlate == -1) {
+                if (digits.get(i) < digits.get(i - 1)) {
+                    valuePlate = digits.get(i - 1);
                     digits.set(i, valuePlate);
                 }
-            }
-            else{
+            } else {
                 digits.set(i, valuePlate);
             }
         }
@@ -92,8 +88,7 @@ public class AOCRunner extends A_AOC {
     }
 
 
-
-    private int applyDoublon(int n){
+    private int applyDoublon(int n) {
         List<Integer> digits = intToList(n);
         int lastDigit = digits.get(digits.size() - 1);
         int beforeLastDigitIndice = digits.size() - 2;
@@ -105,40 +100,40 @@ public class AOCRunner extends A_AOC {
         return Integer.parseInt(concatStr);
     }
 
-    private int applyDoublon2(int number){
+    private int applyDoublon2(int number) {
         int numberTemp = number;
         boolean valid = false;
-        while (!valid){
+        while (!valid) {
             numberTemp++;
             valid = checkDoublon2(numberTemp);
         }
         return numberTemp;
     }
 
-    private boolean checkDoublon(int number){
+    private boolean checkDoublon(int number) {
         return regexDoublon.matcher(Integer.toString(number)).find();
     }
 
-    private boolean checkDoublon2(int number){
+    private boolean checkDoublon2(int number) {
         return regexDoublon2.matcher(Integer.toString(number)).find();
     }
 
 
-    private boolean checkIncrease(int number){
+    private boolean checkIncrease(int number) {
         return regexIncrease.matcher(Integer.toString(number)).find();
     }
 
 
-    private List<Integer> intToList(Integer integer){
+    private List<Integer> intToList(Integer integer) {
         return new java.util.ArrayList<>(Integer.toString(integer).chars().map(c -> c - '0').boxed().toList());
     }
 
-    private String listToInt(List<Integer> listToConcat){
+    private String listToInt(List<Integer> listToConcat) {
         return listToConcat.stream()
                 .map(Object::toString).collect(Collectors.joining(""));
     }
 
-    private List<Integer> parseInput(){
+    private List<Integer> parseInput() {
         return Arrays.stream(inputList.get(0).split("-"))
                 .map(Integer::parseInt)
                 .toList();
