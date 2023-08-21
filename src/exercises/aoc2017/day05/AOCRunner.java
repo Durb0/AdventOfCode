fea9498ee5431b8fcb019a6927d3358c53fe755c
Durb0 <thomasnou@orange.fr>
2023-08-21 14:11:17 +0200

    :construction:init

--- src/exercises/aoc2017/day05/AOCRunner.java
+++ src/exercises/aoc2017/day05/AOCRunner.java
@@ -24,33 +24,6 @@ public class AOCRunner extends A_AOC {
 
     @Override
     public void run() {
-        List<Integer> instructions = initList();
-        solution1 = mazeSolve(instructions.toArray(new Integer[0]), false);
-        solution2 = mazeSolve(instructions.toArray(new Integer[0]), true);
-    }
 
-    private List<Integer> initList() {
-        List<Integer> instructions = new ArrayList<>();
-        for (String line : inputList) {
-            int instruction = Integer.parseInt(line);
-            instructions.add(instruction);
-        }
-        return instructions;
-    }
-
-    private int mazeSolve(Integer[] instructions, boolean crazySolving) {
-        int index = 0;
-        int steps = 0;
-        while (index < instructions.length) {
-            int instruction = instructions[index];
-            if (crazySolving && instruction >= 3) {
-                instructions[index]--;
-            } else {
-                instructions[index]++;
-            }
-            index += instruction;
-            steps++;
-        }
-        return steps;
     }
 }
