7cde19b8508839bf0b1f5fb190844a82a1a6f7d2
Durb0 <thomasnou@orange.fr>
2023-11-21 11:54:15 +0100

    y2021d6

--- src/exercises/aoc2021/day06/AOCRunner.java
+++ src/exercises/aoc2021/day06/AOCRunner.java
@@ -1,6 +1,9 @@
 package exercises.aoc2021.day06;
 
 import utilities.A_AOC;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * <pre>
@@ -10,16 +13,34 @@ import utilities.A_AOC;
  */
 public class AOCRunner extends A_AOC {
 
+    //Création d'une liste fixe de 8 éléments (pour chaque jour)
+    Population population = new Population();
+
     @Override
     public void test() {
         if(isExample) {
-            super.test(0, 0);
+            super.test(5934, 26984457539L);
         } else {
-            super.test(0,0);
+            super.test(353274,1609314870967L);
         }
     }
 
     @Override
     public void run() {
+        this.parseInput();
+        solution1 = this.numberOfFishAfter(80);
+        solution2 = this.numberOfFishAfter(256);
+    }
+
+    private Long numberOfFishAfter(Integer nbDays){
+        Population myPopulation = new Population(this.population);
+        for(int day = 0; day < nbDays; day++){
+            myPopulation = myPopulation.nextDay();
+        }
+        return myPopulation.getNumberOfFish();
+    }
+    private void parseInput(){
+        List<Integer> input = Arrays.stream(this.inputList.get(0).split(",")).map(Integer::parseInt).toList();
+        input.forEach(value -> this.population.set(value, this.population.get(value) + 1));
     }
 }
