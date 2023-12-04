a496f5fabe9c90578bd087d129b877f3f5409ace
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2023-12-04 17:23:22 +0100

    Y2023d1 (#6)
    * Clean code + save input on load
    
    * Add day 5 od 2015
    
    * Add day 5 od 2016
    
    * Add day 5 od 2017
    
    * Clean code
    
    * Faster run if test all
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * 🔧 update configuration for me
    
    * 🚀 y2018d4
    
    * 🚀 y2019d4
    
    * ♻️ y2019d4
    
    * :rocket: y2021d4
    
    * :rocket: y2016d5
    
    * :art:amelioration du validate
    
    * :construction:init
    
    * :sparkles: y2017d5
    
    * :art: maj list
    
    * :art: changement de nom pour les methodes et variables
    
    * :art: ajout d'un pattern strategique
    
    * preparation
    
    * y2021d6
    
    * Ctrl+alt+L
    
    * y2023d1
    
    ---------
    
    Co-authored-by: Manerial <avr1l.dunmare@gmail.com>
    Co-authored-by: thomas.vate <thomas.vate@geodis.com>

---
+++ src/exercises/aoc2023/day01/Number.java
@@ -0,0 +1,11 @@
+package exercises.aoc2023.day01;
+
+public class Number {
+    int intValue;
+    String stringValue;
+
+    public Number(int invValue, String stringValue){
+        this.intValue = invValue;
+        this.stringValue = stringValue;
+    }
+}
