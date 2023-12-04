25cb91aec95d4584c9da84a1bcaddd9b1fe17ca1
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2023-12-04 17:11:36 +0100

    Y2021d6 (#5)
    * Clean code + save input on load
    
    * Add day 5 od 2015
    
    * Add day 5 od 2016
    
    * Add day 5 od 2017
    
    * Clean code
    
    * Faster run if test all
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * Clean code
    
    * 🔧 update configuration for me
    
    * 🚀 y2018d4
    
    * 🚀 y2019d4
    
    * ♻️ y2019d4
    
    * :rocket: y2021d4
    
    * :rocket: y2016d5
    
    * :art:amelioration du validate
    
    * :construction:init
    
    * :sparkles: y2017d5
    
    * :art: maj list
    
    * :art: changement de nom pour les methodes et variables
    
    * :art: ajout d'un pattern strategique
    
    * preparation
    
    * y2021d6
    
    * Ctrl+alt+L
    
    ---------
    
    Co-authored-by: Manerial <avr1l.dunmare@gmail.com>
    Co-authored-by: thomas.vate <thomas.vate@geodis.com>

---
+++ src/exercises/aoc2021/day06/Population.java
@@ -0,0 +1,45 @@
+package exercises.aoc2021.day06;
+
+import lombok.EqualsAndHashCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+@EqualsAndHashCode(callSuper = true)
+public class Population extends ArrayList<Long> {
+
+    public Population() {
+        super(Collections.nCopies(9, 0L));
+    }
+
+    public Population(Population population) {
+        super(population);
+    }
+
+    public void addTo(Integer index, Long value) {
+        this.set(index, this.get(index) + value);
+    }
+
+    public Long getNumberOfFish() {
+        Long count = 0L;
+        for (Long fishs : this) {
+            count += fishs;
+        }
+        return count;
+    }
+
+    public Population nextDay() {
+        Population newDay = new Population();
+        for (int caseIndex = 0; caseIndex < this.size(); caseIndex++) {
+            if (caseIndex == 0) {
+                Long nbOfFish = this.get(0);
+                newDay.addTo(6, nbOfFish);
+                newDay.addTo(8, nbOfFish);
+            } else {
+                newDay.addTo(caseIndex - 1, this.get(caseIndex));
+            }
+        }
+        return newDay;
+    }
+
+}
